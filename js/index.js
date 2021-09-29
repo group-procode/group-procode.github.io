6baf9b77024d7f03ce2025ad6828b76b294110f6
Jaqueline Adan <jaqueline.adan2002@gmail.com>
2021-09-29 20:47:02 -0300

    corrigindo conflitos

--- js/index.js
+++ js/index.js
@@ -1,21 +1,20 @@
-const section = document.querySelector('.features-div')
-const sectionObserver = new IntersectionObserver((entries)=>{
- 
-    entries.forEach(entry=>{
-        console.log(entry)
-        if(!entry.isIntersecting){
-            return;
-        }else{
-            section.classList.add('active')
-            sectionObserver.unobserve(entry.target); 
-        }
-    })
-},section)
-sectionObserver.observe(section)
+const section = document.querySelector('.features-div');
+const sectionObserver = new IntersectionObserver((entries) => {
+
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      section.classList.add('active');
+      sectionObserver.unobserve(entry.target);
+    }
+  })
+}, section);
+
+sectionObserver.observe(section);
 window.onscroll = handleHeaderScroll;
 
 function handleHeaderScroll() {
-  console.log(document.body.scrollTop)
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.querySelector(".header").style.height = "100px";
     document.querySelector(".header-logo img").style.height = "50px";
